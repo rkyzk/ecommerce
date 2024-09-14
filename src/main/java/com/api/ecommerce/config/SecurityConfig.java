0fc98b68326e0a93209d4067e3374154db1b1d01
rkyzk <reikoyazaki@yahoo.com>
2024-09-14 17:12:00 +0900

    add userDetailsService bean in securityConfig

--- src/main/java/com/api/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/api/ecommerce/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.api.ecommerce.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	UserDetailsService userDetailsService;
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -22,6 +27,11 @@ public class SecurityConfig {
 			registry.anyRequest().authenticated();
 		});
 	    return httpSecurity.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return userDetailsService;
 	}
 	
 	@Bean

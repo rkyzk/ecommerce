626e4b648b4cf7d4d0a653d787536362a32d2569
rkyzk <reikoyazaki@yahoo.com>
2024-09-14 16:46:09 +0900

    add userEntity

--- src/main/java/com/api/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/api/ecommerce/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +15,17 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.authorizeHttpRequests(registry->{
-			registry.requestMatchers("/**").permitAll();
+		httpSecurity.csrf(AbstractHttpConfigurer::disable);
+		httpSecurity.authorizeHttpRequests(registry->{
+			registry.requestMatchers("/home").permitAll();
 			registry.requestMatchers("/user/**").hasRole("USER");
 			registry.anyRequest().authenticated();
-		}).build();
+		});
+	    return httpSecurity.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }

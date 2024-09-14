9c9734fd227e737580f7ae81ac2f0129b8394f71
rkyzk <reikoyazaki@yahoo.com>
2024-09-14 17:20:41 +0900

    add authenticationProvider bean in SecurityConfig

--- src/main/java/com/api/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/api/ecommerce/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.api.ecommerce.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,6 +34,14 @@ public class SecurityConfig {
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return userDetailsService;
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder());
+		return provider;
 	}
 	
 	@Bean
